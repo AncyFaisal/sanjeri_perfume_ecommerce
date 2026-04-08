66e098610bfe208f77eab77fbd2c3886357f223c
ancy <ancy.ay@gmailcom>
2026-04-09 03:23:53 +0530

    Add Resend credentials in settings

--- sanjeri_project/sanjeri_project/settings.py
+++ sanjeri_project/sanjeri_project/settings.py
@@ -1,4 +1,4 @@
-﻿"""
+"""
 Django settings for sanjeri_project project.
 """
 
@@ -141,14 +141,12 @@ AUTH_USER_MODEL = 'sanjeri_app.CustomUser'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email configuration - FROM .env
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@sanjeri.com')
+# Email configuration - Resend API
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+ANYMAIL = {
+    "RESEND_API_KEY": os.getenv("RESEND_API_KEY"),
+}
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'support@sanjeriperfume.in')
 
 # Authentication backends
 AUTHENTICATION_BACKENDS = (
